6a07523c0b31ac1fb983bfdb93b946ec93f70fa4
sunwei <sunwei@eqfox.com>
2017-05-15 16:45:34 +0800

    rename to package-readme

--- src/gitignore.js
+++ src/gitignore.js
@@ -2,6 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 export default (dir) => {
-  const content = fs.readFileSync(path.join(dir, '.gitignore'), 'utf8');
-  return content.split('\n').map(line => line.trim()).filter(line => line && line[0] !== '#');
+  if (fs.existsSync(path.join(dir, '.gitignore'))) {
+    const content = fs.readFileSync(path.join(dir, '.gitignore'), 'utf8');
+    return content.split('\n').map(line => line.trim()).filter(line => line && line[0] !== '#');
+  }
+  return [];
 };
